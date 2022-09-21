47b800e943582e510d07600587acfc266606eb01
robolab-kinova <you@example.com>
2022-09-21 15:50:04 +0200

    ventanastoinflux with try catch

--- Ventanas/ventanastoinflux.py
+++ Ventanas/ventanastoinflux.py
@@ -11,20 +11,25 @@ from influxdb import InfluxDBClient
 def on_message(client, userdata, message):
     print("in-message")
     data=json.loads(str(message.payload.decode("utf-8"))) #recogida mensaje
-    informacion = {
-    			'measurement': data['deviceName'],
-    			'tags': {'gatewayID': data['rxInfo'][0]['gatewayID']},
-                   	'fields':{'rssi': float(data['rxInfo'][0]['rssi']),'loRaSNR': float(data['rxInfo'][0]['loRaSNR']),'door':(data['object']['door']),'humidity':float(data['object']['humidity']),'temperature':float(data['object']['temperature'])}
-                  }
+    
+    try:
+        informacion = {
+                        'measurement': data['deviceName'],
+                        'tags': {'gatewayID': data['rxInfo'][0]['gatewayID']},
+                        'fields':{'rssi': float(data['rxInfo'][0]['rssi']),'loRaSNR': float(data['rxInfo'][0]['loRaSNR']),'door':(data['object']['door']),'humidity':float(data['object']['humidity']),'temperature':float(data['object']['temperature'])}
+                    }
 
-#estructura datos a introducir en influxdb
-    json_body = []
-    json_body.append(informacion)
-    print(json_body)
-    cliente = InfluxDBClient(host='158.49.112.127', port=8086) #conexión influxdb
-    cliente.switch_database('ventanas')
-    datoinside = cliente.write_points(json_body, time_precision='s')#inserción datos
-    print(data)
+    #estructura datos a introducir en influxdb
+        json_body = []
+        json_body.append(informacion)
+        print(json_body)
+        cliente = InfluxDBClient(host='158.49.112.127', port=8086) #conexión influxdb
+        cliente.switch_database('ventanas')
+        datoinside = cliente.write_points(json_body, time_precision='s')#inserción datos
+        print(data)
+
+    except:
+        print("Hello")
 
 #conexión a cliente mqtt
 broker_address = "158.49.112.171"
