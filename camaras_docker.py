b5683a129d69ceb1994c6aaebb4f8cbb26799d77
robolab-kinova <you@example.com>
2022-11-16 15:53:56 +0100

    updated to fosca cams

--- camaras_docker.py
+++ camaras_docker.py
@@ -1,11 +1,22 @@
 import cv2
 from yolov7_package import Yolov7Detector
-import time
+import sched, time
+from datetime import datetime
 
 # Camera url list
 cam_list = [
-    "rtsp://guest:smpt00@10.253.247.25:88/videoMain"
+    "rtsp://guest:smpt00@10.253.247.25:88/videoMain", # TIC hall
+    "http://admin:xtrem$@10.253.247.40:5900/mjpg/video.mjpghttp://admin:xtrem$@10.253.247.40:5900/mjpg/video.mjpg", # I1 classroom (Does not work)
+    "http://admin:xtrem$@10.253.246.37/snapshot.cgi", # Norba classroom camera_1
+    "http://admin:xtrem$@10.253.246.38/snapshot.cgi", # Norba classroom camera_2
+    "http://admin:xtrem$@10.253.246.35/snapshot.cgi", # Novell classroom camera_1 (Does not work)
+    "http://admin:xtrem$@10.253.246.36/snapshot.cgi", # Novell classroom camera_2 (Does not work)
+    "rtsp://estudiante:DuRa2020@10.253.246.230/axis-media/media.amp?camera=1", # C7 classroom teacher camera
+    "rtsp://guest:smpt00@10.253.246.23:88/videoMain", # AT Hall
+    "rtsp://robolab1:0pticalflow@158.49.247.195:88/videoMain"
     ]
+
+s = sched.scheduler(time.time,time.sleep)
 
 def get_camera_dir_from_URL(cam_url):
     """
@@ -15,10 +26,15 @@ def get_camera_dir_from_URL(cam_url):
     return ret
 
 def show_video():
+    """
+    Show the video of the camera specificar in the cam_list variable
+    """
     # Get the current camera frame from the address and a boolean value if exists
-    capture = cv2.VideoCapture(cam_list[0])
-    
-    if not capture.isOpened():
+    print(cam_list[8])
+    capture = cv2.VideoCapture(cam_list[8])
+    print("1")
+
+    if capture.isOpened():
         print("Error opening video stream or file")
 
     while(capture.isOpened()):
@@ -33,32 +49,42 @@ def show_video():
 
     capture.release()    
 
-def get_yolo_items():
+def get_yolo_items(sc):
+    """
+    Print the items id of the ones identified by YOLO
+    """
     # Get the current camera frame from the address and a boolean value if exists
-    capture = cv2.VideoCapture(cam_list[0])
+    capture = cv2.VideoCapture(cam_list[8], cv2.CAP_FFMPEG)
 
     if capture.isOpened():
         validate, camera_frame = capture.read()
 
         if validate:
-            cv2.imshow('camera frame', camera_frame)
-            cv2.waitKey(0)
+            # cv2.imshow('camera frame', camera_frame)
+            # cv2.waitKey(0)
             det = Yolov7Detector(traced=False)
             classes, boxes, scores = det.detect(camera_frame) 
-            #Maybe person id = 0
+            #Person_id = 0
             print(classes)
+            print(classes[0].count(0))
     else:
         print("Error opening camera!")
     
-    capture.release()    
+    capture.release()   
+    sc.enter(60, 1, get_yolo_items, (sc,)) #Throw the event each 60 seconds
 
 if __name__ == "__main__":
-    camera_address = get_camera_dir_from_URL(cam_list[0])
-    print(camera_address)
     
-    if __debug__:
-        print('Debug ON')
-    else:
-        print('Debug OFF')
-
+    # while(True):
+        # camera_address = get_camera_dir_from_URL(cam_list[0])
+        # n_people = get_yolo_items().count("0")
+        # print(n_people)
     show_video()
+    now = datetime.now()
+
+    # current_time = now.strftime("%H:%M:%S")
+    # print("Current Time =", current_time)
+    # s.enter(60, 1, get_yolo_items, (s,)) #Throw the event each 60 seconds
+    # s.run()
+    #insert in data base
+
